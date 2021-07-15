92cca503fbeb55a1febc8cbd376087ba4073c0e6
Alexey Inkin <leha@inkin.ru>
2021-07-15 09:39:06 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter-minimal
+
+A minimal Flutter app to start with. It is 4.5 times shorter than the default Flutter Demo.
---
+++ android/app/src/main/kotlin/com/example/minimal/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.minimal
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
